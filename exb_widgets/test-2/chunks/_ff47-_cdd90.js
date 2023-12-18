9483dead91b485a483f395e6e067e1685f99051f
Junshan Liu <junshan_liu@esri.com>
2023-12-18 11:22:29 +0800

    new widget

---
+++ exb_widgets/test-2/chunks/_ff47-_cdd90.js
@@ -0,0 +1,24 @@
+(self["webpackChunkexb_client"] = self["webpackChunkexb_client"] || []).push([["_ff47-_cdd90"],{
+
+/***/ "?ff47":
+/*!********************!*\
+  !*** fs (ignored) ***!
+  \********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ }),
+
+/***/ "?cdd9":
+/*!**********************!*\
+  !*** path (ignored) ***!
+  \**********************/
+/***/ (() => {
+
+/* (ignored) */
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoid2lkZ2V0cy9jaHVua3MvX2ZmNDctX2NkZDkwLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQUE7Ozs7Ozs7Ozs7QUNBQSIsInNvdXJjZXMiOlsid2VicGFjazovL2V4Yi1jbGllbnQvaWdub3JlZHxEOlxcd29ya19yb290XFxkZXZ0b3BpYVxcRXhwZXJpZW5jZUJ1aWxkZXJcXGNsaWVudFxcZXh0ZW5zaW9uc1xcd2lkZ2V0c1xcYXJjZ2lzXFxhbmFseXNpc1xcbm9kZV9tb2R1bGVzXFxAYXJjZ2lzXFxnZW9lbnJpY2htZW50LWNvbXBvbmVudHNcXGRpc3RcXGVzbXxmcyIsIndlYnBhY2s6Ly9leGItY2xpZW50L2lnbm9yZWR8RDpcXHdvcmtfcm9vdFxcZGV2dG9waWFcXEV4cGVyaWVuY2VCdWlsZGVyXFxjbGllbnRcXGV4dGVuc2lvbnNcXHdpZGdldHNcXGFyY2dpc1xcYW5hbHlzaXNcXG5vZGVfbW9kdWxlc1xcQGFyY2dpc1xcZ2VvZW5yaWNobWVudC1jb21wb25lbnRzXFxkaXN0XFxlc218cGF0aCJdLCJzb3VyY2VzQ29udGVudCI6WyIvKiAoaWdub3JlZCkgKi8iLCIvKiAoaWdub3JlZCkgKi8iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
