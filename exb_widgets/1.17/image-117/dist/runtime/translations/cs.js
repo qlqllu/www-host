2499f3aba230483000039bc2a427634952d41d43
chenBolan <813938067@qq.com>
2025-03-03 10:03:50 +0800

    Add new custom widget

---
+++ exb_widgets/1.17/image-117/dist/runtime/translations/cs.js
@@ -0,0 +1,1 @@
+System.register([],(function(e){return{execute:function(){e({_widgetLabel:"Sn\xedmek",imageChooseShape:"Tvar",imageCrop:"O\u0159ez",previousImage:"P\u0159edchoz\xed sn\xedmek",nextImage:"Dal\u0161\xed sn\xedmek"})}}}));
